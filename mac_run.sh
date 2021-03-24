134d62fc65e4ddccae61fbd2e3b74d59f50a6b2a
junhui-f <fjh828@gmail.com>
2021-03-24 13:21:44 +0800

    Updated run files

--- mac_run.sh
+++ mac_run.sh
@@ -1,2 +1,3 @@
-g++ -std=c++17 main.cpp -o mac -I "SDL2/include_mac" -L "SDL2/lib_mac" -lSDL2
-./mac
+#g++ -std=c++17 main.cpp -o rMac -I "SDL2/include_mac" -L "SDL2/lib_mac" -lSDL2 -lSDL2_ttf
+g++ -std=c++17 main.cpp -o rMac -lSDL2main -lSDL2 -lSDL2_ttf -lstdc++
+./rMac
