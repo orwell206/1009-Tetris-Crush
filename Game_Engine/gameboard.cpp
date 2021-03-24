bf7f8dc47e4338b298e9e96780752ef209277a55
Chia Wei <yattiekarin@gmail.com>
2021-03-25 02:18:51 +0800

    Popup message after gameover

--- Game_Engine/gameboard.cpp
+++ Game_Engine/gameboard.cpp
@@ -12,19 +12,21 @@
 const u8 FRAMES_PER_DROP[] = {48, 43, 38, 33, 28, 23, 18, 13, 8, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1};
 const f32 TARGET_SECONDS_PER_FRAME = 1.f / 60.f;
 
-
-enum Game_Phase{
+enum Game_Phase
+{
 	GAME_PHASE_PLAY,
 	GAME_PHASE_LINE,
 	GAME_PHASE_GAMEOVER,
 };
-enum Text_Align{
-		TEXT_ALIGN_LEFT,
-		TEXT_ALIGN_CENTER,
-		TEXT_ALIGN_RIGHT
+enum Text_Align
+{
+	TEXT_ALIGN_LEFT,
+	TEXT_ALIGN_CENTER,
+	TEXT_ALIGN_RIGHT
 };
 
-class GameBoard {
+class GameBoard
+{
 public:
 	u8 gameboard[WIDTH * HEIGHT];
 	u8 lines[HEIGHT];
@@ -42,67 +44,70 @@ public:
 	s32 line_count;
 	s32 points;
 
-
-	void set_player(Player &player){
+	void set_player(Player &player)
+	{
 		this->player = &player;
 	}
 
 	void draw_text(SDL_Renderer *renderer,
-            TTF_Font *font,
-            const char *text,
-            s32 x, s32 y,
-            Text_Align alignment,
-            Color color){
-    SDL_Color sdl_color = SDL_Color { color.r, color.g, color.b, color.a };
-    SDL_Surface *surface = TTF_RenderText_Solid(font, text, sdl_color);
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+				   TTF_Font *font,
+				   const char *text,
+				   s32 x, s32 y,
+				   Text_Align alignment,
+				   Color color)
+	{
+		SDL_Color sdl_color = SDL_Color{color.r, color.g, color.b, color.a};
+		SDL_Surface *surface = TTF_RenderText_Solid(font, text, sdl_color);
+		SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    SDL_Rect rect;
-    rect.y = y;
-    rect.w = surface->w;
-    rect.h = surface->h;
-    switch (alignment)
-    {
-    case TEXT_ALIGN_LEFT:
-        rect.x = x;
-        break;
-    case TEXT_ALIGN_CENTER:
-        rect.x = x - surface->w / 2;
-        break;
-    case TEXT_ALIGN_RIGHT:
-        rect.x = x - surface->w;
-        break;
-    }
+		SDL_Rect rect;
+		rect.y = y;
+		rect.w = surface->w;
+		rect.h = surface->h;
+		switch (alignment)
+		{
+		case TEXT_ALIGN_LEFT:
+			rect.x = x;
+			break;
+		case TEXT_ALIGN_CENTER:
+			rect.x = x - surface->w / 2;
+			break;
+		case TEXT_ALIGN_RIGHT:
+			rect.x = x - surface->w;
+			break;
+		}
 
-    SDL_RenderCopy(renderer, texture, 0, &rect);
-    SDL_FreeSurface(surface);
-    SDL_DestroyTexture(texture);
-}
-	void render_clearline_graphics(const GameBoard *gameboard, SDL_Renderer *renderer){
+		SDL_RenderCopy(renderer, texture, 0, &rect);
+		SDL_FreeSurface(surface);
+		SDL_DestroyTexture(texture);
+	}
+	void render_clearline_graphics(const GameBoard *gameboard, SDL_Renderer *renderer)
+	{
 		Color highlight_color = color(0xFF, 0xFF, 0xFF, 0xFF);
-		if (gameboard->gamePhase == GAME_PHASE_LINE){
-			for (s32 row = 0; row < HEIGHT; row++){
-				if (gameboard->lines[row]){
+		if (gameboard->gamePhase == GAME_PHASE_LINE)
+		{
+			for (s32 row = 0; row < HEIGHT; row++)
+			{
+				if (gameboard->lines[row])
+				{
 					s32 x = 0;
 					s32 y = row * GRID_SIZE;
 					fill_rect(renderer, x, y, WIDTH * GRID_SIZE, GRID_SIZE, highlight_color);
 				}
 			}
 		}
-
 	}
 
-
-	void render_game(const GameBoard *gameboard, SDL_Renderer *renderer,TTF_Font *font){
+	void render_game(const GameBoard *gameboard, SDL_Renderer *renderer, TTF_Font *font)
+	{
 		draw_on_board(renderer, gameboard->gameboard, WIDTH, HEIGHT, 0, 0);
-	
 
-		if (gameboard->gamePhase == GAME_PHASE_PLAY){
+		if (gameboard->gamePhase == GAME_PHASE_PLAY)
+		{
 			draw_tetromino(renderer, &gameboard->tetrominoPiece, 0, 0);
 		}
 
 		render_clearline_graphics(gameboard, renderer);
-
 
 		//temp for rendering score text on GUI
 		char buffer[4096];
@@ -114,34 +119,39 @@ public:
 		//draw_text(renderer, font, "test", x, y, TEXT_ALIGN_CENTER, highlight_color);
 		//gameboard->points = 0;
 		snprintf(buffer, sizeof(buffer), "POINTS: %d", gameboard->points);
-   		draw_text(renderer, font, buffer, x, y, TEXT_ALIGN_LEFT, highlight_color);
+		draw_text(renderer, font, buffer, x, y, TEXT_ALIGN_LEFT, highlight_color);
 	}
 
-
-	void draw_on_board(SDL_Renderer *renderer, const u8 *gameboard, s32 width, s32 height, s32 offset_x, s32 offset_y){
-		for (s32 row = 0; row < height; row++){
-			for (s32 col = 0; col < width; col++){
+	void draw_on_board(SDL_Renderer *renderer, const u8 *gameboard, s32 width, s32 height, s32 offset_x, s32 offset_y)
+	{
+		for (s32 row = 0; row < height; row++)
+		{
+			for (s32 col = 0; col < width; col++)
+			{
 				u8 value = get_matrix(gameboard, width, row, col);
 				draw_cell(renderer, row, col, value, offset_x, offset_y);
 			}
 		}
 	}
 
-
-	void draw_tetromino(SDL_Renderer *renderer, const TetrominoPieceState *tetrominoPiece, s32 offset_x, s32 offset_y){
+	void draw_tetromino(SDL_Renderer *renderer, const TetrominoPieceState *tetrominoPiece, s32 offset_x, s32 offset_y)
+	{
 		const Tetromino *tempTetromino = TETROMINOS.tetromino_list + tetrominoPiece->tetromino_index;
-		for (s32 row = 0; row < tempTetromino->side; row++){
-			for (s32 col = 0; col < tempTetromino->side; col++){
+		for (s32 row = 0; row < tempTetromino->side; row++)
+		{
+			for (s32 col = 0; col < tempTetromino->side; col++)
+			{
 				u8 value = get_tetromino(tempTetromino, row, col, tetrominoPiece->rotation);
-				if (value){
+				if (value)
+				{
 					draw_cell(renderer, row + tetrominoPiece->offset_row, col + tetrominoPiece->offset_col, value, offset_x, offset_y);
 				}
 			}
 		}
 	}
 
-
-	void draw_cell(SDL_Renderer *renderer, s32 row, s32 col, u8 value, s32 offset_x, s32 offset_y){
+	void draw_cell(SDL_Renderer *renderer, s32 row, s32 col, u8 value, s32 offset_x, s32 offset_y)
+	{
 		Color base_color = BASE_COLORS[value];
 		Color light_color = LIGHT_COLORS[value];
 		Color dark_color = DARK_COLORS[value];
@@ -152,54 +162,55 @@ public:
 
 		fill_rect(renderer, x, y, GRID_SIZE, GRID_SIZE, dark_color);
 		fill_rect(renderer, x + edge, y, GRID_SIZE - edge, GRID_SIZE - edge, light_color);
-		fill_rect(renderer, x + edge, y + edge, GRID_SIZE - edge * 2, GRID_SIZE -edge * 2, base_color);
+		fill_rect(renderer, x + edge, y + edge, GRID_SIZE - edge * 2, GRID_SIZE - edge * 2, base_color);
 	}
 
-
-	inline u8 get_tetromino(const Tetromino *tetromino, s32 row, s32 col, s32 rotation){
+	inline u8 get_tetromino(const Tetromino *tetromino, s32 row, s32 col, s32 rotation)
+	{
 		s32 side = tetromino->side;
-		switch(rotation){
-			case 0:
-				return tetromino->matrix[row * side + col];
-			case 1:
-				return tetromino->matrix[(side - col - 1) * side + row];
-			case 2:
-				return tetromino->matrix[(side - row - 1) * side + (side - col - 1)];
-			case 3:
-				return tetromino->matrix[col * side + (side - row - 1)];
+		switch (rotation)
+		{
+		case 0:
+			return tetromino->matrix[row * side + col];
+		case 1:
+			return tetromino->matrix[(side - col - 1) * side + row];
+		case 2:
+			return tetromino->matrix[(side - row - 1) * side + (side - col - 1)];
+		case 3:
+			return tetromino->matrix[col * side + (side - row - 1)];
 		}
 		return 0;
 	}
 
-
-	inline u8 check_if_row_filled(const u8 *values, s32 width, s32 row){
-		for (s32 col = 0; col < width; col++){
-			if (!get_matrix(values, width, row, col)){
+	inline u8 check_if_row_filled(const u8 *values, s32 width, s32 row)
+	{
+		for (s32 col = 0; col < width; col++)
+		{
+			if (!get_matrix(values, width, row, col))
+			{
 				return 0;
 			}
 		}
 		return 1;
 	}
 
-
-	inline u8 check_if_row_empty(const u8 *values, s32 width, s32 row){
-		for (s32 col = 0; col < width; col++){
-			if (get_matrix(values, width, row, col)){
+	inline u8 check_if_row_empty(const u8 *values, s32 width, s32 row)
+	{
+		for (s32 col = 0; col < width; col++)
+		{
+			if (get_matrix(values, width, row, col))
+			{
 				return 0;
 			}
 		}
 		return 1;
 	}
 
-
-
-
-
-
-
-	s32 find_lines(const u8 *values, s32 width, s32 height, u8 *lines_out){
+	s32 find_lines(const u8 *values, s32 width, s32 height, u8 *lines_out)
+	{
 		s32 count = 0;
-		for (s32 row = 0; row < height; row++){
+		for (s32 row = 0; row < height; row++)
+		{
 			u8 filled = check_if_row_filled(values, width, row);
 			lines_out[row] = filled;
 			count += filled;
@@ -207,54 +218,61 @@ public:
 		return count;
 	}
 
-
-	void clear_lines(u8 *values, s32 width, s32 height, const u8 *lines){
+	void clear_lines(u8 *values, s32 width, s32 height, const u8 *lines)
+	{
 		s32 src_row = height - 1;
-		for (s32 dst_row = height - 1; dst_row >= 0; dst_row--){
-			while (src_row > 0 && lines[src_row]){
+		for (s32 dst_row = height - 1; dst_row >= 0; dst_row--)
+		{
+			while (src_row > 0 && lines[src_row])
+			{
 				src_row--;
 			}
 
-			if (src_row < 0){
+			if (src_row < 0)
+			{
 				memset(values + dst_row * width, 0, width);
 			}
-			else {
+			else
+			{
 				memcpy(values + dst_row * width, values + src_row * width, width);
 				src_row--;
 			}
 		}
 	}
 
-
-	inline u8 get_matrix(const u8 *values, s32 width, s32 row, s32 col){
+	inline u8 get_matrix(const u8 *values, s32 width, s32 row, s32 col)
+	{
 		s32 index = row * width + col;
 		return values[index];
 	}
 
-
-	inline void set_matrix(u8 *values, s32 width, s32 row, s32 col, u8 value){
+	inline void set_matrix(u8 *values, s32 width, s32 row, s32 col, u8 value)
+	{
 		s32 index = (row * width + col);
 		values[index] = value;
 	}
 
+	int update_game(GameBoard *gameboard, const InputState *input, SDL_Renderer *renderer, TTF_Font *font)
+	{
+		switch (gameboard->gamePhase)
+		{
+		case GAME_PHASE_PLAY:
+			update_gameplay(gameboard, input);
+			break;
 
-	void update_game(GameBoard *gameboard, const InputState *input){
-		switch(gameboard->gamePhase){
-			case GAME_PHASE_PLAY:
-				update_gameplay(gameboard, input);
-				break;
+		case GAME_PHASE_LINE:
+			update_gameline(gameboard);
+			break;
 
-			case GAME_PHASE_LINE:
-				update_gameline(gameboard);
-				break;
-				
-			case GAME_PHASE_GAMEOVER:
-				break;
+		case GAME_PHASE_GAMEOVER:
+			int returnValue = game_Over(renderer, font);
+			return returnValue;
 		}
+		return 2;
 	}
 
-
-	void fill_rect(SDL_Renderer *renderer, s32 x, s32 y, s32 width, s32 height, Color color){
+	void fill_rect(SDL_Renderer *renderer, s32 x, s32 y, s32 width, s32 height, Color color)
+	{
 		SDL_Rect rect = {};
 		rect.x = x;
 		rect.y = y;
@@ -264,30 +282,38 @@ public:
 		SDL_RenderFillRect(renderer, &rect);
 	}
 
-
-	bool check_tetromino_valid(const TetrominoPieceState *tetromino_piece, const u8 *gameboard, s32 width, s32 height){
+	bool check_tetromino_valid(const TetrominoPieceState *tetromino_piece, const u8 *gameboard, s32 width, s32 height)
+	{
 		const Tetromino *tetromino = TETROMINOS.tetromino_list + tetromino_piece->tetromino_index;
 		assert(tetromino);
 
-		for (s32 row = 0; row < tetromino->side; row++){
-			for (s32 col = 0; col < tetromino->side; col++){
+		for (s32 row = 0; row < tetromino->side; row++)
+		{
+			for (s32 col = 0; col < tetromino->side; col++)
+			{
 				u8 value = get_tetromino(tetromino, row, col, tetromino_piece->rotation);
-				if (value > 0){
+				if (value > 0)
+				{
 					s32 board_row = tetromino_piece->offset_row + row;
 					s32 board_col = tetromino_piece->offset_col + col;
-					if (board_row < 0){
+					if (board_row < 0)
+					{
 						return false;
 					}
-					if (board_row >= height){
+					if (board_row >= height)
+					{
 						return false;
 					}
-					if (board_col < 0){
+					if (board_col < 0)
+					{
 						return false;
 					}
-					if (board_col >= width){
+					if (board_col >= width)
+					{
 						return false;
 					}
-					if (get_matrix(gameboard, width, board_row, board_col)){
+					if (get_matrix(gameboard, width, board_row, board_col))
+					{
 						return false;
 					}
 				}
@@ -296,27 +322,30 @@ public:
 		return true;
 	}
 
-
-	void spawn_tetromino(GameBoard *gameboard){
+	void spawn_tetromino(GameBoard *gameboard)
+	{
 		gameboard->tetrominoPiece = {};
 		gameboard->tetrominoPiece.tetromino_index = (u8)random_tetromino_index(0, TETROMINOS.tetromino_shape_count);
 		gameboard->tetrominoPiece.offset_col = WIDTH / 2;
 		gameboard->nextDropTime = gameboard->time + get_time_to_next_tetromino_drop(gameboard->level);
 	}
 
-
-	int random_tetromino_index(s32 min, s32 max){
+	int random_tetromino_index(s32 min, s32 max)
+	{
 		s32 range = max - min;
-		return min + rand() % range; 
-
+		return min + rand() % range;
 	}
 
-	void merge_tetrimino_on_board(GameBoard *gameboard, Player *player){
+	void merge_tetrimino_on_board(GameBoard *gameboard, Player *player)
+	{
 		const Tetromino *tetromino = TETROMINOS.tetromino_list + gameboard->tetrominoPiece.tetromino_index;
-		for (s32 row = 0; row < tetromino->side; row++){
-			for (s32 col = 0; col < tetromino->side; col++){
+		for (s32 row = 0; row < tetromino->side; row++)
+		{
+			for (s32 col = 0; col < tetromino->side; col++)
+			{
 				u8 value = get_tetromino(tetromino, row, col, gameboard->tetrominoPiece.rotation);
-				if (value){
+				if (value)
+				{
 					s32 board_row = gameboard->tetrominoPiece.offset_row + row;
 					s32 board_col = gameboard->tetrominoPiece.offset_col + col;
 					set_matrix(gameboard->gameboard, WIDTH, board_row, board_col, value); // problem
@@ -325,10 +354,11 @@ public:
 		}
 	}
 
-
-	inline bool drop_tetromino(GameBoard *gameboard, Player *player){
+	inline bool drop_tetromino(GameBoard *gameboard, Player *player)
+	{
 		++gameboard->tetrominoPiece.offset_row;
-		if (!check_tetromino_valid(&gameboard->tetrominoPiece, gameboard->gameboard, WIDTH, HEIGHT)){
+		if (!check_tetromino_valid(&gameboard->tetrominoPiece, gameboard->gameboard, WIDTH, HEIGHT))
+		{
 			--gameboard->tetrominoPiece.offset_row;
 			merge_tetrimino_on_board(gameboard, player);
 			spawn_tetromino(gameboard);
@@ -338,18 +368,19 @@ public:
 		return true;
 	}
 
-
-
-	inline f32 get_time_to_next_tetromino_drop(s32 level){
-		if (level > 29){
+	inline f32 get_time_to_next_tetromino_drop(s32 level)
+	{
+		if (level > 29)
+		{
 			level = 29;
 		}
 		return FRAMES_PER_DROP[level] * TARGET_SECONDS_PER_FRAME;
 	}
 
-
-	void update_gameline(GameBoard *gameboard){
-		if (gameboard->time >= gameboard->highlighted_end_time){
+	void update_gameline(GameBoard *gameboard)
+	{
+		if (gameboard->time >= gameboard->highlighted_end_time)
+		{
 			clear_lines(gameboard->gameboard, WIDTH, HEIGHT, gameboard->lines);
 			gameboard->line_count += gameboard->pending_line_count;
 			//For debugging
@@ -357,54 +388,109 @@ public:
 			//Temp adding of score
 			gameboard->points += 20 * gameboard->pending_line_count;
 			gameboard->gamePhase = GAME_PHASE_PLAY;
-			
 		}
 	}
 
-
-	void update_gameplay(GameBoard *gameboard, const InputState *input){
+	void update_gameplay(GameBoard *gameboard, const InputState *input)
+	{
 
 		TetrominoPieceState tetrominoPiece = gameboard->tetrominoPiece;
-		if (input->da > 1){
+		if (input->da > 1)
+		{
 			--tetrominoPiece.offset_col;
 		}
-		if (input->dd > 1){
+		if (input->dd > 1)
+		{
 			++tetrominoPiece.offset_col;
 		}
-		if (input->dw > 1){
+		if (input->dw > 1)
+		{
 			tetrominoPiece.rotation = (tetrominoPiece.rotation + 1) % 4;
 		}
 
-		if (check_tetromino_valid(&tetrominoPiece, gameboard->gameboard, WIDTH, HEIGHT)){
+		if (check_tetromino_valid(&tetrominoPiece, gameboard->gameboard, WIDTH, HEIGHT))
+		{
 			gameboard->tetrominoPiece = tetrominoPiece;
 		}
 
-		if (input->ds > 1){
+		if (input->ds > 1)
+		{
 			drop_tetromino(gameboard, player);
 		}
 
-		if (input->dspace > 1){
-			while (drop_tetromino(gameboard, player));
+		if (input->dspace > 1)
+		{
+			while (drop_tetromino(gameboard, player))
+				;
 		}
 
-
-		while (gameboard->time >= gameboard->nextDropTime){
+		while (gameboard->time >= gameboard->nextDropTime)
+		{
 			drop_tetromino(gameboard, player);
 		}
 
 		s32 line_count = find_lines(gameboard->gameboard, WIDTH, HEIGHT, gameboard->lines);
-		//temp for adding pending line count 
+		//temp for adding pending line count
 		gameboard->pending_line_count = find_lines(gameboard->gameboard, WIDTH, HEIGHT, gameboard->lines);
-		if (line_count > 0){
+		if (line_count > 0)
+		{
 			gameboard->gamePhase = GAME_PHASE_LINE;
 			gameboard->highlighted_end_time = gameboard->time + 0.5f;
 		}
 
 		s32 game_over_row = 0;
 
-		if (!check_if_row_empty(gameboard->gameboard, WIDTH, game_over_row)){
+		if (!check_if_row_empty(gameboard->gameboard, WIDTH, game_over_row))
+		{
 			gameboard->gamePhase = GAME_PHASE_GAMEOVER;
 		}
 	}
+	int game_Over(SDL_Renderer *renderer, TTF_Font *font)
+	{
+		int flag;
+		const SDL_MessageBoxButtonData buttons[] = {
+			{/* .flags, .buttonid, .text */ 0, 0, "Exit"},
+			{SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 1, "Restart NOT WORKING ATM"},
+		};
+		const SDL_MessageBoxColorScheme colorScheme = {
+			{/* .colors (.r, .g, .b) */
+			 /* [SDL_MESSAGEBOX_COLOR_BACKGROUND] */
+			 {255, 0, 0},
+			 /* [SDL_MESSAGEBOX_COLOR_TEXT] */
+			 {0, 255, 0},
+			 /* [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] */
+			 {255, 255, 0},
+			 /* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
+			 {0, 0, 255},
+			 /* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
+			 {255, 0, 255}}};
+		const SDL_MessageBoxData messageboxdata = {
+			SDL_MESSAGEBOX_INFORMATION,			  /* .flags */
+			NULL,								  /* .window */
+			"Game Over - Choose Option",		  /* .title */
+			"Continue or Exit?\n Highest Player", /* .message */
+			SDL_arraysize(buttons),				  /* .numbuttons */
+			buttons,							  /* .buttons */
+			&colorScheme						  /* .colorScheme */
+		};
+		int buttonid;
+		if (SDL_ShowMessageBox(&messageboxdata, &buttonid) < 0)
+		{
+			SDL_Log("error displaying message box");
+			return 1;
+		}
+		if (buttonid == -1)
+		{
+			SDL_Log("no selection");
+		}
+		else if (buttonid == 0)
+		{
+			return 0;
+		}
+		else if (buttonid == 1)
+		{
+			return 1;
+		}
+		return 0;
+	}
 };
-	
--- main.cpp
+++ main.cpp
@@ -11,76 +11,93 @@
 #include "Game_Engine/colours.hpp"
 #include "Game_Engine/gameboard.cpp"
 
-int main(){
-	if (SDL_Init(SDL_INIT_VIDEO) < 0){
+int main()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
 		return -1;
 	}
 	//For TTF font
 	if (TTF_Init() < 0)
-    {
-        return 2;
-    }
+	{
+		return 2;
+	}
+	bool restart = false;
+	do
+	{
+		SDL_Window *window = SDL_CreateWindow("Tetris Crush Team 42", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 780, 570, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	SDL_Window *window = SDL_CreateWindow("Tetris Crush Team 42", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 780, 570, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		Player player;
 
-	Player player;
+		GameBoard gameboard = {};
+		gameboard.set_player(player);
 
+		InputState input = {};
 
-	GameBoard gameboard = {};
-	gameboard.set_player(player);
+		gameboard.spawn_tetromino(&gameboard);
 
-	InputState input = {};
+		bool quit = false;
+		//Open font
+		const char *font_name = "novem___.ttf";
+		TTF_Font *font = TTF_OpenFont(font_name, 24);
+		while (!quit)
+		{
+			gameboard.time = SDL_GetTicks() / 1000.0f;
 
-	gameboard.spawn_tetromino(&gameboard);
+			SDL_Event e;
+			while (SDL_PollEvent(&e) != 0)
+			{
 
-	bool quit = false;
-	//Open font
-	const char *font_name = "novem___.ttf";
-	TTF_Font *font = TTF_OpenFont(font_name, 24);
-	while (!quit){
-		gameboard.time = SDL_GetTicks() / 1000.0f;
-		
+				if (e.type == SDL_QUIT)
+				{
+					quit = true;
+					restart = true;
+				}
+			}
 
-		SDL_Event e;
-		while (SDL_PollEvent(&e) != 0){
-			if (e.type == SDL_QUIT){
+			s32 key_count;
+			const u8 *key_states = SDL_GetKeyboardState(&key_count);
+
+			InputState prev_input = input;
+
+			// P1 Controls
+			input.a = key_states[SDL_SCANCODE_A];
+			input.d = key_states[SDL_SCANCODE_D];
+			input.w = key_states[SDL_SCANCODE_W];
+			input.s = key_states[SDL_SCANCODE_S];
+			input.space = key_states[SDL_SCANCODE_SPACE];
+
+			input.da = input.a - prev_input.a;
+			input.dd = input.d - prev_input.d;
+			input.dw = input.w - prev_input.w;
+			input.ds = input.s - prev_input.s;
+			input.dspace = input.space - prev_input.space;
+
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+			SDL_RenderClear(renderer);
+
+			int returnValue = gameboard.update_game(&gameboard, &input, renderer, font);
+			if (returnValue == 0)
+			{
+
+				quit = true;
+				restart = true;
+			}
+			else if (returnValue == 1)
+			{
+
+				restart = false;
 				quit = true;
 			}
+			gameboard.render_game(&gameboard, renderer, font);
+
+			SDL_RenderPresent(renderer);
 		}
-
-		s32 key_count;
-		const u8 *key_states = SDL_GetKeyboardState(&key_count);
-
-		InputState prev_input = input;
-
-
-		// P1 Controls
-		input.a = key_states[SDL_SCANCODE_A];
-		input.d = key_states[SDL_SCANCODE_D];
-		input.w = key_states[SDL_SCANCODE_W];
-		input.s = key_states[SDL_SCANCODE_S];
-		input.space = key_states[SDL_SCANCODE_SPACE];
-
-
-		input.da = input.a - prev_input.a;
-		input.dd = input.d - prev_input.d;
-		input.dw = input.w - prev_input.w;
-		input.ds = input.s - prev_input.s;
-		input.dspace = input.space - prev_input.space;
-
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		//Close font after game close
+		TTF_CloseFont(font);
+		SDL_DestroyRenderer(renderer);
 		SDL_RenderClear(renderer);
-
-		gameboard.update_game(&gameboard, &input);
-		gameboard.render_game(&gameboard, renderer,font);
-
-		SDL_RenderPresent(renderer);
-	}
-	//Close font after game close
-	TTF_CloseFont(font);
-	SDL_DestroyRenderer(renderer);
-	
-	SDL_Quit();
-	return 0;
+		SDL_Quit();
+	} while (!restart);
 }
