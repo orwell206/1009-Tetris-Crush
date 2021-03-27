7664dfbab1633e20cf402da368d069c4a8553427
junhui-f <fjh828@gmail.com>
2021-03-27 18:34:29 +0800

    Next tetromino working (cout)

--- Game_Engine/tetromino_piece_state.cpp
+++ Game_Engine/tetromino_piece_state.cpp
@@ -8,4 +8,17 @@ class TetrominoPieceState{
 		s32 offset_row;
 		s32 offset_col;
 		s32 rotation;
+
+		void resetState();
+		u8 get_tetromino_index();
+		void set_tetromino_index(u8 tetromino_index);
+		s32 get_offset_row();
+		void set_offset_row(s32 offset_row);
 };
+
+void TetrominoPieceState::resetState() {
+	this->tetromino_index = 0;
+	this->offset_row = 0;
+	this->offset_col = 0;
+	this->rotation = 0;
+}
