8bdf527e68560f8d6293964e3b9ffaacad69219a
shufenlim <61367983+shufenlim@users.noreply.github.com>
2021-04-05 23:39:59 +0800

    Comment Done

--- Audio_Engine/sound_effect.cpp
+++ Audio_Engine/sound_effect.cpp
@@ -1,11 +1,17 @@
 #include "audio.cpp"
 
-class SoundEffect: public Audio{
+class SoundEffect: public Audio
+{
 public:
     void play();    // Overriding - to play the audio once only
 };
 
-void SoundEffect::play() {
+void SoundEffect::play()
+{
+    /*
+        Desc: Play the audio once only.
+        Params: None.
+    */
     Mix_PlayChannel(-1, audioChunk,0);  // (channel,audio,loop=0)
     Mix_HaltChannel(1);
 }
